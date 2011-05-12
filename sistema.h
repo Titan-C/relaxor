037e22cd5c1832ccc185010d1c9e5a22dc8b0ba3
Óscar Nájera <najera.oscar@gmail.com>
2011-05-12 15:46:12 -0500

    Cambiar header del sistema

--- sistema.h
+++ sistema.h
@@ -2,23 +2,36 @@
 #define SISTEMA_H
 
 #include <vector>
+#include <gsl/gsl_rng.h>
 
 class Sistema
 {
   private:
+    unsigned int dimension, L;
+    double Pol=1;
+    
+    
     std::vector<int> sigma;			// Arreglo de spines dipolares
     std::vector<int> sum_sigma;			// Suma de spines individuales
+    std::vector<double> mu_H;			// Arreglo de proyeciones de momento al eje del campo
   
     std::vector< std::vector<int> > G;		// Topología de primeros vecinos
     std::vector< std::vector<double> > J;	// Energías de intercambio primeros vecinos
-    
+
   public:
     // Constructor y destructor
-    Sistema(unsigned int lado, unsigned int dimension = 3, bool polarizar = true);
+    Sistema(unsigned int lado, gsl_rng * rng, unsigned double r_max = 2, unsigned int dim = 3, bool polarizar = true);
     ~Sistema();
+    
+    void init(gsl_rng* rng, unsigned double r_max, bool polarizar);
+    
+    
     
     
     
 };
+// Operaciones necesarias para tratar al sistema
+void condborde ( vector <double>& R, int L);
+double dot(const vector<double>& a, const vector<double>& b);
 
 #endif // SISTEMA_H
