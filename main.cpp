6cba3eba9fba3ab79921660d062cd8b2ad231a09
Óscar Nájera <najera.oscar@gmail.com>
2011-05-13 04:21:31 -0500

    cambio temperaturas para probar

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
   gsl_rng * rng = gsl_rng_alloc (gsl_rng_taus);
   
   unsigned int L=8, Niter=400, numexps = 1;
-  double T=2;/*
+  double T=5;/*
   cin>>L;
   cin>> Niter;
   cin>>T;*/
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
     //ejecutar una corrida en el tiempo correspondiente a Niter  
     numexps += relaxor.experimento(T, 0, Niter, grabar , rng);
     if (grabar)
-      T-=0.1;
+      T-=0.2;
     
   }while(T>0);
   
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
     //Arreglo de temperaturas
   temp.resize(mediciones);
   for(unsigned int i = 0; i<temp.size(); i++)
-    temp[i] = T + (mediciones-i)*0.1;
+    temp[i] = T + (mediciones-i)*0.2;
   
   S_frozen.resize(mediciones);
   for(unsigned int i=0; i< S_frozen.size(); i++){
