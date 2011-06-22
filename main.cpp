0ba9f939c0d34d1ae9fcf6df6432f1ddc1f861a0
Óscar Nájera <najera.oscar@gmail.com>
2011-06-22 16:17:31 -0500

    ordenar estructura. Ejecutar función para reproducir gráficos del paper guia

--- main.cpp
+++ main.cpp
@@ -21,6 +21,12 @@ int main(int argc, char **argv) {
   clock_t cl_start = clock();
   time(&start);
   
+  //vaciar archivo de datos en cada ejecución
+  file_wipe("log");
+  file_wipe("sum_sigma_time.dat");
+  file_wipe("sum_sigma_conf.dat");
+  file_wipe("energy_log.dat");
+  
   gsl_rng * rng = gsl_rng_alloc (gsl_rng_taus);
 
   unsigned int L=8, numexps = 10, Equi_iter=1000, Exp_iter= 400;
@@ -29,11 +35,6 @@ int main(int argc, char **argv) {
 
   Sistema relaxor(L, Exp_iter, rng);
 
-  //vaciar archivo de datos en cada ejecución
-  file_wipe("log");
-  file_wipe("sum_sigma_time.dat");
-  file_wipe("sum_sigma_conf.dat");
-  file_wipe("energy_log.dat");
   cout<<"Energía inicial = "<<relaxor.total_E(0)<<endl;
   vector<double> campos, temperaturas;
   temp_array(temperaturas, relaxor.DeltaJ, T, dT);
@@ -50,11 +51,13 @@ int main(int argc, char **argv) {
 
   // Analisis de datos
   procesar(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos);
+  //Graficos
+  graficos(relaxor.DeltaJ, temperaturas, campos);
 
   time(&end);
   cout<<difftime(end,start)<<endl;
   cout<<(double)(clock()-cl_start)/CLOCKS_PER_SEC;
-  system("gnuplot ../plots.p");
+  system("gnuplot ../plotsf.p");
   
   return 0;
 }
