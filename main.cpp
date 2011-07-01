7102d1107c3761fb42b2b475d656bc0172b0ab0c
Óscar Nájera <najera.oscar@gmail.com>
2011-07-01 09:26:34 -0500

    Implemento los varios experimentos que necesito:   Enfriar multicampo   calentar desde polarizado y despolarizado con campos significativos   enfriar desde T<Tf sin campo

--- main.cpp
+++ main.cpp
@@ -22,12 +22,12 @@ int main(int argc, char **argv) {
   time(&start);
   
   //vaciar archivo de datos en cada ejecución
-system("rm *.dat");
+  system("rm *.dat");
   
   gsl_rng * rng = gsl_rng_alloc (gsl_rng_taus);
 
-  unsigned int L=8, numexps = 4, Equi_iter=1000, Exp_iter= 400;
-  double T=2.5,dT = 0.1, H=2, dH=0.1;
+  unsigned int L=12, numexps = 8, Equi_iter=1000, Exp_iter= 400;
+  double T=5,dT = 0.1, H=2, dH=0.1;
   gsl_rng_set(rng, time(NULL) );
 
   Sistema relaxor(L, Exp_iter, rng);
@@ -39,24 +39,62 @@ system("rm *.dat");
   for(unsigned int E=0;E < campos.size(); E++){
     for(unsigned int n = 0; n < numexps; n++){
       for(unsigned int T = 0; T < temperaturas.size(); T++){
-	relaxor.experimento(temperaturas[T], 2.5*campos[E], Equi_iter, false , rng);
-	relaxor.experimento(temperaturas[T], 2.5*campos[E], Exp_iter, true , rng);
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Equi_iter, false , rng,"cool");
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Exp_iter, true , rng,"cool");
       }
     }
   }
-
   // Analisis de datos
-  procesar(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos);
-  //Graficos
-  graficos(relaxor.DeltaJ, temperaturas, campos);
-
+  Sus_proc(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos,"cool");
+  Pol_proc(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos,"cool");
+  plot_data_sus(relaxor.DeltaJ, temperaturas, campos, "cool");
+  
+//Calentar desde pol
+  Equi_iter = 50;
+  temperaturas = temp_array(relaxor.DeltaJ, 3, dT, true);
+  campos.assign(2,0.2*relaxor.DeltaJ);
+  campos[1]=0.6*relaxor.DeltaJ;
+  for(unsigned int E=0;E < campos.size(); E++){
+    for(unsigned int n = 0; n < numexps; n++){
+      relaxor.init_pol(rng,true);
+      for(unsigned int T = 0; T < temperaturas.size(); T++){
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Equi_iter, false , rng,"heatpol");
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Exp_iter, true , rng,"heatpol");
+      }
+    }
+  }
+  // Analisis de datos
+  Pol_proc(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos,"heatpol");
+//Calentar desde unpol
+  for(unsigned int E=0;E < campos.size(); E++){
+    for(unsigned int n = 0; n < numexps; n++){
+      relaxor.init_pol(rng,false);
+      for(unsigned int T = 0; T < temperaturas.size(); T++){
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Equi_iter, false , rng,"heatunpol");
+	relaxor.experimento(temperaturas[T], 2.5*campos[E], Exp_iter, true , rng,"heatunpol");
+      }
+    }
+  }
+  // Analisis de datos
+  Pol_proc(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos,"heatunpol");
+  
+//Enfriar T<Tf
+  Equi_iter = 400; string file="coolpol"; campos.assign(1,0);
+  for(unsigned int i=0;i<4;i++){
+    temperaturas = temp_array(relaxor.DeltaJ, 1-i*0.2, dT, true);
+    for(unsigned int n = 0; n < numexps; n++){
+      relaxor.init_pol(rng,true);
+      for(unsigned int T = 0; T < temperaturas.size(); T++){
+	relaxor.experimento(temperaturas[T], 0, Equi_iter, false , rng,file.c_str());
+	relaxor.experimento(temperaturas[T], 0, Exp_iter, true , rng,file.c_str());
+      }
+    }
+    Pol_proc(numexps, Exp_iter, L, relaxor.DeltaJ, temperaturas, campos ,file.c_str());
+    file+="_t";
+  }
   time(&end);
   cout<<difftime(end,start)<<endl;
   cout<<(double)(clock()-cl_start)/CLOCKS_PER_SEC;
-  system("gnuplot ../plotsf.p");
-  system("rename .dat _1.dat *.dat");
-  
-  
-  
   return 0;
 }
+
