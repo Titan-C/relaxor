5fa08fda706c0d91f81370ebd5c771747f34d04f
Óscar Nájera <najera.oscar@gmail.com>
2012-03-01 13:33:49 -0500

    Incorporar nuevas funciones al fitter. Ya Compara contra la simulación, pero aún no presenta resultados con buen orden, ni genera nuevas llamadas a la simulación para generar nuevos datos

--- fitter.py
+++ fitter.py
@@ -38,15 +38,46 @@ def plotter(equation):
   T = equation.dataCache.allDataCacheDictionary['IndependentData'][0]
   E = equation.dataCache.allDataCacheDictionary['DependentData']
   a,b,c,d = equation.solvedCoefficients
-  x=arange(-350,T.max()*1.1,T.max()/150)
+  x=arange(100,T.max()*1.1,T.max()/150)
   F=a*(x-b)/(x*x+c*x+d)
   plot(T,E,'o')
   plot(x,F)
+  
+def scaleFitter(fit_eq):
+  record = 0.0
+  files=glob.glob('/home/oscar/fineresults/sus_cool_J1*.dat')
+  for file in files:
+    S=open(file)
+    data = S.read()
+    equation = pyeq2.Models_2D.UserDefinedFunction.UserDefinedFunction('SSQABS','Default', fit_eq)
+    pyeq2.dataConvertorService().ConvertAndSortColumnarASCII(data, equation, False)
+    equation.estimatedCoefficients = [100,0.008]
+    pyeq2.solverService().SolveUsingSimplex(equation)
+    
+    #imprimir resultados
+    equation.CalculateCoefficientAndFitStatistics()
+    if equation.r2 > record:
+      record = equation.r2
+      print "Material", file
+      print 'R-squared:',  equation.r2, equation.solvedCoefficients
+    
+  
+  
+def recorder(equation,file):
+  f=open('Expdata','a')
+  f.write(file+'\t')
+  f.write(str(equation.r2)+'\t')
+  for coef in equation.solvedCoefficients:
+    f.write(str(coef)+'\t')
+  f.write('\n')
+  f.close()
 
-if __name__ == "__main__":
+def test_experiment():
   files=glob.glob('data/P*K.dat')
+  
   for file in files:
     eq = data_fitter(file)
+    recorder(eq,file)
     print file, eq.solvedCoefficients
     plotter(eq)
   xlabel('Temperature [$\\Delta J /k_B$]')
@@ -54,3 +85,20 @@ if __name__ == "__main__":
   title('Simulation Data & fits')
   legend(('DATA','FIT'))
   show()
+  
+def get_equation(info):
+  if float(info[3]) < 0:
+    fit_eq = '+'+ info[3][1:]
+  else:
+    fit_eq = '-'+ info[3]
+  fit_eq = 'u/j*'+info[2]+'*(j*X'+fit_eq+')/(j**2*X*X+'+info[4]+'*j*X+'+info[5]+')'
+  return fit_eq
+  
+if __name__ == "__main__":
+  Exper=open('Expdata')
+  for exper in Exper:
+    print '\n Para el material ', exper.split()[0]
+    fit_eq = get_equation( exper.split() )
+    scaleFitter(fit_eq)
+  Exper.close()
+    
