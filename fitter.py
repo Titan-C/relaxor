176ba93f4a971d234d925ee34ced7cf0dd3c7c45
Óscar Nájera <najera.oscar@gmail.com>
2012-03-01 06:18:06 -0500

    Simplificaciones fitter.py

--- fitter.py
+++ fitter.py
@@ -3,8 +3,6 @@
 import os, sys, inspect
 
 # ensure pyeq2 can be imported
-if os.path.join(sys.path[0][:sys.path[0].rfind(os.sep)], '../..') not in sys.path:
-    sys.path.append(os.path.join(sys.path[0][:sys.path[0].rfind(os.sep)], '../..'))
 import pyeq2
 from numpy import *
 from pylab import *
@@ -16,7 +14,7 @@ def data_fitter(file):
   #Busca los coeficientes
   equation = pyeq2.Models_2D.BioScience.MembraneTransport('SSQABS')
   pyeq2.dataConvertorService().ConvertAndSortColumnarASCII(data,equation,False)
-#  equation.estimatedCoefficients = [9e4,-1.3e2,-8e2,2.5e5]
+  equation.estimatedCoefficients = [9e4,-13,-8e2,2.5e5]
   equation.Solve()
   equation.CalculateCoefficientAndFitStatistics()
   print 'R-squared:',  equation.r2
@@ -29,7 +27,7 @@ def data_fitterOff(file):
   #Busca los coeficientes
   equation = pyeq2.Models_2D.BioScience.MembraneTransport('SSQABS','Offset')
   pyeq2.dataConvertorService().ConvertAndSortColumnarASCII(data,equation,False)
-  equation.estimatedCoefficients = [9e5,-1.3e2,-9e2,2.5e5,200]
+  equation.estimatedCoefficients = [9e5,-13,-9e2,2.5e5,200]
   equation.Solve()
   equation.CalculateCoefficientAndFitStatistics()
   print 'R-squared:',  equation.r2
@@ -40,30 +38,19 @@ def plotter(equation):
   T = equation.dataCache.allDataCacheDictionary['IndependentData'][0]
   E = equation.dataCache.allDataCacheDictionary['DependentData']
   a,b,c,d = equation.solvedCoefficients
-  x=arange(0,T.max()*1.1,T.max()/150)
+  x=arange(-350,T.max()*1.1,T.max()/150)
   F=a*(x-b)/(x*x+c*x+d)
   plot(T,E,'o')
   plot(x,F)
 
 if __name__ == "__main__":
-  files=glob.glob('/home/oscar/workplace/Relaxor/ICTPdat/sus*.dat')
+  files=glob.glob('data/P*K.dat')
   for file in files:
     eq = data_fitter(file)
-    print eq.solvedCoefficients
-    #plotter(eq)
-    #eq0 = data_fitterOff(file)
-    #print eq0.solvedCoefficients
+    print file, eq.solvedCoefficients
     plotter(eq)
   xlabel('Temperature [$\\Delta J /k_B$]')
   ylabel('Electric susceptibility $\\chi$')
   title('Simulation Data & fits')
   legend(('DATA','FIT'))
   show()
-  
-#Fitted Parameters:
-    #a = 1.2370050415987713E+05
-    #b = 2.6765810051540325E+02
-    #c = -9.9125857400687005E+02
-    #d = 2.5241678263901567E+05
-#R-squared: 0.987034132705
-#Root Mean Squared Error (RMSE): 143.790205244 
