f796d8312a0f48942cd46dad6cecfe66f84383f5
Óscar Nájera <najera.oscar@gmail.com>
2012-03-13 09:25:32 -0500

    Cambiar nombres a las funciones e incluir la función para grafiar los ajustes

--- plotter.py
+++ plotter.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from numpy import *
 from pylab import *
 from sys import argv
 from glob import glob
 
-def plot_pol(file):
-  rho,E,tau = prop(file)
+def simpolPlot(file):
+  rho,E,tau = identifier(file)
   data = genfromtxt(file)
   T = data[:,0]
   p = data[:,1]
@@ -13,19 +12,35 @@ def plot_pol(file):
   errorbar(T,p,yerr=pe,label='E='+E+' $\\tau=$'+tau)
   return rho,E,tau
 
-def plot_sus(file):
-  rho,E,tau = prop(file)
+def simsusPlot(file):
+  rho,E,tau = identifier(file)
   data = genfromtxt(file)
   T = data[:,0]
   X = data[:,1]
   Xe = data[:,2]
   I = data[:,3]
   Ie = data[:,4]
-  plot(T,X,'o-',label='E='+E+' $\\tau=$'+tau)
-  #errorbar(T,X,yerr=Xe,label='E='+E+' $\\tau=$'+tau)
+  #plot(T,X,'o-',label='E='+E+' $\\tau=$'+tau)
+  errorbar(T,X,yerr=Xe,label='E='+E+' $\\tau=$'+tau)
   return rho,E,tau
+  
+def expsusPlot(file):
+  data = genfromtxt(file)
+  T = data[:,0]
+  X = data[:,1]
+  Xe = data[:,2]*X
+  plot(T,X,T,Xe)
 
-def prop(file):
+def fittedPlot(equation):
+  T = equation.dataCache.allDataCacheDictionary['IndependentData'][0]
+  E = equation.dataCache.allDataCacheDictionary['DependentData']
+  a,b,c,d = equation.solvedCoefficients
+  x=arange(T.min()*0.8,T.max()*1.1,T.max()/150)
+  F=a*(x-b)/(x*x+c*x+d)
+  plot(T,E,'o')
+  plot(x,F)
+
+def identifier(file):
   rho = file.find('_p')
   E = file.find('_E')
   tau = file.find('_t')
@@ -40,7 +55,7 @@ def prop(file):
 def generator(path):
   files = glob(path)
   for file in files:
-    rho = plot_sus(file)[0]
+    rho = simsusPlot(file)[0]
   xlabel('Temperature [$\\Delta J /k_B$]')
   ylabel('Electric susceptibility $\\chi$')
   title('Simulation Data for $\\rho=$'+rho)
