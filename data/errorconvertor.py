a07cd7cf661681f783de8c111b767a493b355f93
Óscar Nájera <najera.oscar@gmail.com>
2012-03-29 12:02:05 -0500

    Script que calcula la cantidad de error presente en las mediciones experimentales usando la medida tan(d) de los datos

---
+++ data/errorconvertor.py
@@ -0,0 +1,16 @@
+from numpy import array, genfromtxt, savetxt
+from glob import glob
+
+def include_err(file,errpow):
+    data=genfromtxt(file)
+    T=data[:,0]
+    E=data[:,1]
+    d=data[:,2]
+    err=(100/E/d)**(errpow)
+    new=array([T,E,err,d])
+    savetxt('er'+str(errpow)+file,new.T,fmt='%3.6g',delimiter='\t')
+    
+if __name__ == "__main__":
+  files = glob('P*dat')
+  for file in files:
+    include_err(file,1/4.0)
