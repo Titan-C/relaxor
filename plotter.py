3cf74830f0d5359739713ff46c02a8e6f5eae257
Óscar Nájera <najera.oscar@gmail.com>
2012-04-23 18:27:55 -0500

    Graficador con zoom para historial de energía del sistema Graficadores de polarización, susceptibildad y enegía para grupos de archivos Labelers para gráficos

--- plotter.py
+++ plotter.py
@@ -2,34 +2,33 @@
 from pylab import *
 from sys import argv
 from glob import glob
+from utils import zoom_effect02
 
-def elogPlot(file,fmt='+'):
+def HlogPlot(file,stl='+'):
   H=genfromtxt(file)
-  plot(H,fmt)
-  xlabel('Iteraciones $[MCS/dipolo]$')
-  ylabel(u'Energía del  sistema $[\Delta J]$')
-  title(u'Evolución de la energía en un proceso de enfriamiento')
-  show()
+  plot(H,stl,label=legendSet(file))
 
-def simpolPlot(file):
+def polPlot(file,stl='-o', error=False):
   lb=legendSet(file)
   data = genfromtxt(file)
   T = data[:,0]
   p = data[:,1]
-  pe = data[:,2]
-  errorbar(T,p,yerr=pe,label=lb)
-  return rho,E,tau
+  if not error:
+    plot(T,p,stl,label=lb)
+  else:
+    pe = data[:,2]
+    errorbar(T,p,yerr=pe,fmt=stl,label=lb)
 
-def susPlot(file,fmt='-o', error=False):
+def susPlot(file,stl='-o', error=False):
   lb=legendSet(file)
   data = genfromtxt(file)
   T = data[:,0]
   X = data[:,1]
   if not error:
-    plot(T,X,fmt,label=lb)
+    plot(T,X,stl,label=lb)
   else:
     std = data[:,3]
-    errorbar(T,X,yerr=std,label=lb)
+    errorbar(T,X,yerr=std,fmt=stl, label=lb)
 
 def fittedPlot(equation,file):
   T = equation.dataCache.allDataCacheDictionary['IndependentData'][0]
@@ -48,10 +47,20 @@ def scalefittedPlot(equation,coefs,file):
   F=u/j*a*(j*x-b)/(j**2*x*x+c*j*x+d)
   plot(T,E,'o',x,F,label=legendSet(file))
 
-def axisLabel():
+def susLabel():
   xlabel('Temperatura [$\\Delta J /k_B$]')
-  ylabel('Electric susceptibility $\\chi$')
+  ylabel(u'Susceptibilidad dieléctrica $\\chi$')
   title('Data & fits')
+
+def polLabel():
+  xlabel('Temperatura [$\\Delta J /k_B$]')
+  ylabel(u'Polarización normada del sistema $[\\frac{1}{N\\mu}]$')
+  title(u'Polarización global en un proceso de enfriamiento\nCampo externo nulo')
+
+def HlogLabel():
+  xlabel('Iteraciones $[MCS/dipolo]$')
+  ylabel(u'Energía del sistema $[\Delta J]$')
+  suptitle(u'Evolución de la energía en un proceso de enfriamiento\nCampo externo nulo')
 
 def simIdentifier(file):
   rho = file.find('_p')
@@ -68,17 +77,44 @@ def simIdentifier(file):
 def legendSet(file):
   if file.find('_p') > 0:
     rho,E,tau = simIdentifier(file)
-    return '$\\rho=$'+rho+' E='+E+' $\\tau=$'+tau
+    return '$\\rho=$'+rho#+' E='+E+' $\\tau=$'+tau
     
   else:
     mat = file.find('P')
     return file[mat:mat+5]+' '+file[mat+5:-4]
 
-def filesusPlot(path,fmt='-o',error=False):
+def filesusPlot(path,stl='-o',error=False):
   files = sort(glob(path))
   for file in files:
-    susPlot(file,fmt,error)
-  axisLabel()
+    susPlot(file,stl,error)
+  susLabel()
+  legend()
+  show()
+
+def filepolPlot(path,stl='-o',error=False):
+  files = sort(glob(path))
+  for file in files:
+    polPlot(file,stl,error)
+  polLabel()
+  legend()
+  show()
+
+def fileHlogPlot(path,stl='+',error=False):
+  '''Grafica el historial de la energía del sistema y hace
+     un zoom a un intervalo predefinido'''
+  files = sort(glob(path))
+  ax1 = plt.subplot(211)
+  ax1.set_xlim(5.8*3350, 6.3*3350)
+  ax1.set_ylim(-11200, -8400)
+  ax1.annotate(u'$Equilibración$', xy=(6*3350+50,-9000),xytext=(6*3350+50, -9000))
+  plt.axvspan(6*3350, 6*3350+350, facecolor='r', alpha=0.25)
+  for file in files:
+    HlogPlot(file,stl)
+  ax2 = plt.subplot(212)
+  for file in files:
+    HlogPlot(file,stl)
+  zoom_effect02(ax1, ax2)
+  HlogLabel()
   legend()
   show()
 
