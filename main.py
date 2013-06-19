674ac554208ed925231673c10a5fdbf03dab377a
Óscar Nájera <najera.oscar@gmail.com>
2013-06-19 16:56:27 -0500

    Extend main.py argparse. Split temperatures into 3 separate variables

--- main.py
+++ main.py
@@ -10,13 +10,22 @@ parser.add_argument('-n', '--numexps', metavar='N',
 		    help='Number of sampling experiments')
 parser.add_argument('-p', '--rho', type=float, nargs='*',
 		    default=0, help='Mean Ferroelectricity')
-parser.add_argument('-T', '--temperatures', metavar='T', type=float,
-		    nargs=3, default=[7,0.2,1],
-		    help='Temperature range of experiment. Corresponds to [Ti dT Tf]')
-parser.add_argument('-E', '--fields', metavar='E0', type=float, nargs='*',
+parser.add_argument('-E', '--field', metavar='E0', type=float, nargs='*',
 		    default=0.1, help='External Electric Field amplitude')
 parser.add_argument('-t', '--tau', metavar='t', type=float, nargs='*',
 		    default=100, help='External Electric Field period')
+parser.add_argument('-Ti', metavar='T', type=float,
+		    nargs=1, default=7.15,
+		    help='Starting temperature of experiment')
+parser.add_argument('-Tf', metavar='T', type=float,
+		    nargs=1, default=0.1,
+		    help='Finishing temperature of experiment')
+parser.add_argument('-dT', metavar='T', type=float,
+		    nargs=1, default=-0.5,
+		    help='Temperature step during experiment')
+parser.add_argument('--Nexp', metavar='N', type=int, nargs=2,
+		    default=[3000,100], help='Time steps for Experiment and Equilibration')
+
 
 if __name__ == "__main__":
   args=parser.parse_args()
