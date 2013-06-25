c77b1fdfcd5d096d36a12d57d09465c4cfe086bd
Óscar Nájera <najera.oscar@gmail.com>
2013-06-24 19:25:37 -0500

    add eval_sus for computing susceptibility of given experiment. Also include a needed simulations estimator

--- experiment.py
+++ experiment.py
@@ -2,6 +2,8 @@ import libmat
 import numpy as np
 import re
 from pylab import show,plot,errorbar,figure
+from scipy.integrate import simps
+from os import path, remove
 
 #def stepCalibrator(Niter,tau,min_periods):
     #periods = Niter/tau
@@ -48,6 +50,27 @@ def eval_frozen(filename, frozen=[1,0.9]):
 
   return slow_dipoles, thermostat
 
+def eval_sus(filename):
+  info = getLabel(filename)
+  thermostat = np.arange(float(info['Ti']), float(info['Tf'])-0.01, float(info['dT']))
+  with open(filename, 'rb') as f:
+    pol_log=np.fromfile(f,dtype=np.double).reshape( -1, thermostat.size, int(info['Eiter']) )
+
+  pol=pol_log.mean(axis=2)
+  pol_err=pol_log.std(axis=2)
+
+  periods = int(info['Eiter'])/int(info['tau'])
+  t=np.arange(0, 2*np.pi*periods, 2*np.pi/int(info['tau']) )
+  cos_wave=np.cos(t)
+  sin_wave=np.sin(t)
+  susRe=simps(cos_wave*pol_log)/int(info['Eiter'])/float(info['E'])
+  susIm=simps(sin_wave*pol_log)/int(info['Eiter'])/float(info['E'])
+
+  plot(thermostat, pol.mean(axis=0), 'k*-',thermostat, np.abs(pol).mean(axis=0))
+  figure()
+  plot(thermostat, susRe.mean(axis=0), thermostat, susIm.mean(axis=0))
+  return pol,pol_err
+  
 def susGUI(filename, frozen=[0.9, 0.8, 0.6]):
   """Calculate dielectric susceptibility X=(1-p)/T
      where p:proportion of slow dipoles & T:temperature"""
@@ -58,3 +81,21 @@ def susGUI(filename, frozen=[0.9, 0.8, 0.6]):
   for i in range(len(slow_dipoles)):
     plot(thermostat,(1-slow_dipoles[i].mean(axis=0))/thermostat,'*')
   show()
+
+def requiredSimulations(numexps, filename):
+  try:
+    size = path.getsize(filename)
+  except:
+    return numexps
+
+  info = getLabel(filename)
+  thermostat = np.arange(float(info['Ti']), float(info['Tf']), float(info['dT']))
+  n = size/8./float(info['Eiter'])/thermostat.size
+  if int(n) == n:
+    if numexps - n > 0:
+      return numexps - n
+    else:
+      return 0
+  else:
+    remove(filename)
+    return numexps
