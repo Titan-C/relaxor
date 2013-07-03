39964703592dbcc9bd800811c5a8525d1b4a0e48
Óscar Nájera <najera.oscar@gmail.com>
2013-07-03 17:30:01 -0500

    Python experiment performs experiments and is compliant with previous cpp implemantation anr pylint

--- experiment.py
+++ experiment.py
@@ -1,115 +1,164 @@
-import libmat
+# -*- coding: utf-8 -*-
+"""
+@author: Óscar Nájera
+
+Functions to perform experiments and data analysis for simulated relaxors
+"""
+from __future__ import division
 import numpy as np
+import libmat as lm
 import re
-from pylab import show,plot,errorbar,figure
+from pylab import plot, errorbar
 from scipy.integrate import simps
 from os import path, remove
 
-#def stepCalibrator(Niter,tau,min_periods):
-    #periods = Niter/tau
-    #if periods > min_periods:
-        #return periods*tau
-    #else:
-        #return min_periods*tau
 
-def genLabel(name, L, rho, field, tau, Ti, Tf, dT, Eiter, Qiter):
-  """Generate string to label simulation files"""
-  label = name + '_N' + str(L*L*L)
-  label+= '_p' + str(rho) + '_E' + str(field) + '_t' + str(tau)
-  label+= '_Ti'+ str(Ti) + '_Tf'+ str(Tf) + '_dT'+ str(dT)
-  label+= '_X' + str(Eiter) + '_Q' + str(Qiter)
-  return label
+def steps_calibrator(setup):
+    """Stablishes proper amount of MCS during simulation
+       edits setup for later labeling and returns steps interval"""
+    periods = setup.Eiter // setup.tau
+    if periods > setup.osc:
+        setup.Eiter = periods * setup.tau
+    else:
+        setup.Eiter = setup.osc * setup.tau
 
-def getLabel(filename):
-  """Extract from filename simulation setup"""
-  pat = r'_N(?P<N>\d+)'
-  pat+= r'_p(?P<rho>[\d\.]+)_E(?P<E>[\d\.]+)_t(?P<tau>\d+)'
-  pat+= r'_Ti(?P<Ti>[\d\.]+)_Tf(?P<Tf>[\d\.]+)_dT(?P<dT>[-+\d\.]+)'
-  pat+= r'_X(?P<Eiter>\d+)_Q(?P<Qiter>\d+)'
-  return re.search(pat, filename).groupdict()
+    if setup.Qiter > setup.Eiter:
+        setup.Qiter = setup.Eiter
 
-def buildTemp(experiment):
-  """Generate numpy array for temperatures"""
-  return np.arange(float(experiment['Ti']), float(experiment['Tf']), float(experiment['dT']))
+    return np.arange(0, 2*np.pi*periods, 2*np.pi / setup.tau)
 
-def eval_frozen(filename, frozen=[1,0.9]):
-  """Evaluate frozen dipoles proportion for determinate
-     experiment(filename) given the list of thresholds
-     to consider a dipole frozen
-     eval_frozen(filename,[1,0.95])"""
-  experiment = getLabel(filename)
-  thermostat = buildTemp(experiment)
-  with open(filename, 'rb') as f:
-    sigma=np.fromfile(f,dtype=np.int32)/float(experiment['Eiter'])
-  sig=np.abs(sigma).reshape(-1, thermostat.size, int(experiment['N']))
+def gen_label(setup):
+    """Generate string to label simulation files"""
 
-  slow_dipoles=[]
-  for bound in frozen:
-    slow_dipoles.append( np.sum( sig>=bound, axis=2 ) / float( experiment['N'] ) )
+    label  = setup.name + '_N' + str(setup.L**3)
+    label += '_p'+str(setup.rho) +'_E'+str(setup.field) +'_t'+str(setup.tau)
+    label += '_Ti'+str(setup.Ti) +'_Tf'+str(setup.Tf) +'_dT'+str(setup.dT)
+    label += '_X' + str(setup.Eiter) + '_Q' + str(setup.Qiter)
+    return label
 
-  fro=figure('fro')
-  for i in xrange(len(slow_dipoles)):
-    errorbar( thermostat, slow_dipoles[i].mean(axis=0), slow_dipoles[i].std(axis=0) )
+def get_label(filename):
+    """Extract from filename simulation setup"""
+    pat  = r'_N(?P<N>\d+)'
+    pat += r'_p(?P<rho>[\d\.]+)_E(?P<E>[\d\.]+)_t(?P<tau>\d+)'
+    pat += r'_Ti(?P<Ti>[\d\.]+)_Tf(?P<Tf>[\d\.]+)_dT(?P<dT>[-+\d\.]+)'
+    pat += r'_X(?P<Eiter>\d+)_Q(?P<Qiter>\d+)'
+    experiment = re.search(pat, filename).groupdict()
+    for key in ['N', 'Eiter', 'Qiter']:
+        experiment[key] = int(experiment[key])
+    for key in ['rho', 'E', 'tau', 'Ti', 'Tf', 'dT']:
+        experiment[key] = float(experiment[key])
 
-  return slow_dipoles, thermostat
+    return experiment
 
-def getPolLog(filename):
-  experiment = getLabel(filename)
-  thermostat =  buildTemp(experiment)
-  with open(filename, 'rb') as f:
-    pol_log=np.fromfile(f,dtype=np.double).reshape( -1, thermostat.size, int(info['Eiter']) )
-  return pol_log, thermostat, experiment
+def build_temp(experiment):
+    """Generate numpy array for temperatures"""
+    temp = np.arange( experiment['Ti'], experiment['Tf'], experiment['dT'] )
+    return temp
+
+def eval_frozen(filename, frozen = None):
+    """Evaluate frozen dipoles proportion for determinate
+       experiment(filename) given the list of thresholds
+       to consider a dipole frozen
+       eval_frozen(filename,[1,0.95])"""
+    if frozen is None:
+        frozen = [1, 0.9]
+    experiment = get_label(filename)
+    thermostat = build_temp(experiment)
+    with open(filename, 'rb') as datf:
+        sigma = np.fromfile(datf, dtype=np.int32) / experiment['Eiter']
+    sig = np.abs(sigma).reshape(-1, thermostat.size, experiment['N'])
+
+    slow_dipoles = []
+    for bound in frozen:
+        slow_dipoles.append( np.sum(sig>=bound, axis=2) / experiment['N'] )
+
+    for i in xrange(len(slow_dipoles)):
+        errorbar( thermostat, slow_dipoles[i].mean(axis=0),
+                 slow_dipoles[i].std(axis=0) )
+
+    return slow_dipoles, thermostat
+
+def get_pol_log(filename):
+    """Get polarization data and experiment setup from file"""
+    experiment = get_label(filename)
+    thermostat =  build_temp(experiment)
+    with open(filename, 'rb') as datf:
+        pol_log = np.fromfile(datf, dtype=np.double)
+        pol_log = pol_log.reshape( -1, thermostat.size, experiment['Eiter'] )
+    return pol_log, thermostat, experiment
 
 def eval_pol(filename):
-  """Calculate polarization"""
-  pol_log, thermostat, experiment = getPolLog(filename)
-  pol=pol_log.mean(axis=2)
-  pol_err=pol_log.std(axis=2)
+    """Calculate polarization"""
+    pol_log, thermostat = get_pol_log(filename)[:2]
+    pol  = pol_log.mean(axis=2)
+    pol_err = pol_log.std(axis=2)
 
-  plot(thermostat, pol.mean(axis=0), 'k*-',thermostat, np.abs(pol).mean(axis=0))  
+    plot(thermostat, pol.mean(axis=0), 'k*-',
+         thermostat, np.abs(pol).mean(axis=0))
 
-  return pol, pol_err, thermostat
+    return pol, pol_err, thermostat
 
 def eval_sus(filename):
-  """Calculate dielectric susceptibility according to Liu paper"""
-  pol_log, thermostat, experiment = getPolLog(filename)
+    """Calculate dielectric susceptibility according to Liu paper"""
+    pol_log, thermostat, experiment = get_pol_log(filename)
 
-  periods = int(experiment['Eiter'])/int(experiment['tau'])
-  t=np.arange(0, 2*np.pi*periods, 2*np.pi/int(experiment['tau']) )
-  cos_wave=np.cos(t)
-  sin_wave=np.sin(t)
+    periods = experiment['Eiter'] // experiment['tau']
+    steps = np.arange(0, 2*np.pi*periods, 2*np.pi / experiment['tau'] )
+    cos_wave = np.cos(steps)
+    sin_wave = np.sin(steps)
 
-  susRe=simps(cos_wave*pol_log)/int(experiment['Eiter'])/float(experiment['E'])
-  susIm=simps(sin_wave*pol_log)/int(experiment['Eiter'])/float(experiment['E'])
+    sus_re = simps(cos_wave*pol_log)/experiment['Eiter']/experiment['E']
+    sus_im = simps(sin_wave*pol_log)/experiment['Eiter']/experiment['E']
 
-  plot(thermostat, susRe.mean(axis=0), thermostat, susIm.mean(axis=0))
+    plot(thermostat, sus_re.mean(axis=0), thermostat, sus_im.mean(axis=0))
 
-  return susRe, susIm, thermostat
-  
-def susGUI(filename, frozen=[0.9, 0.8, 0.6]):
-  """Calculate dielectric susceptibility X=(1-p)/T
-     where p:proportion of slow dipoles & T:temperature"""
-  slow_dipoles, thermostat = eval_frozen(filename, frozen)
+    return sus_re, sus_im, thermostat
 
-  for i in range(len(slow_dipoles)):
-    plot(thermostat,(1-slow_dipoles[i].mean(axis=0))/thermostat,'*')
+def susGUI(filename, frozen = None):
+    """Calculate dielectric susceptibility X=(1-p)/T
+       where p:proportion of slow dipoles & T:temperature"""
+    if frozen is None:
+        frozen = [0.9, 0.8, 0.6]
+    slow_dipoles, thermostat = eval_frozen(filename, frozen)
 
-def requiredSimulations(numexps, filename):
-  """Calculate the number of needed remaining simulations in order
-     to reach the required number"""
-  try:
-    size = path.getsize(filename)
-  except:
-    return numexps
+    for i in range(len(slow_dipoles)):
+        plot(thermostat,(1-slow_dipoles[i].mean(axis=0))/thermostat,'*')
 
-  info = getLabel(filename)
-  thermostat = np.arange(float(info['Ti']), float(info['Tf']), float(info['dT']))
-  n = size/8./float(info['Eiter'])/thermostat.size
-  if int(n) == n:
-    if numexps - n > 0:
-      return numexps - n
+def required_simulations(numexps, experiment_label):
+    """Calculate the number of needed remaining simulations in order
+       to reach the required number"""
+    filename = 'log_pol_' + experiment_label + '.dat'
+
+    try:
+        size = path.getsize(filename)
+    except OSError:
+        return numexps
+
+    experiment = get_label(filename)
+    thermostat = build_temp(experiment)
+    count = size/8/experiment['Eiter']/thermostat.size
+    if int(count) == count:
+        if numexps - count > 0:
+            return numexps - int(count)
+        else:
+            return 0
     else:
-      return 0
-  else:
-    remove(filename)
-    return numexps
+        remove(filename)
+        return numexps
+
+def do_experiment(setup):
+    """Perform experiment accoding to setup"""
+    exp_label = gen_label(setup)
+    print exp_label
+
+    num = required_simulations(setup.numexps, exp_label)
+    if num > 0:
+        relaxor = lm.Material(setup.L, setup.rho, exp_label,
+                            setup.pol, setup.logH, setup.logS)
+        steps = steps_calibrator(setup)
+        field = lm.double_vector()
+        field[:] = setup.field * np.cos(steps)
+        temperatures = lm.double_vector()
+        temperatures[:] = build_temp(vars(setup))
+        print relaxor.oven(num, setup.Qiter, temperatures,
+                           field, setup.pol)
