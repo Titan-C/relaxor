eaa01e7f06f21e70f0c3f5615df64323654c9e9c
Óscar Nájera <najera.oscar@gmail.com>
2013-07-05 19:37:57 -0500

    Docstring for sigmahist and pylint compliance

--- plots.py
+++ plots.py
@@ -9,7 +9,9 @@ from analysis import get_sigmas_log
 from pylab import hist
 
 def sigmahist(filename):
+    """Plots histogram of dipoles permanence i a certain orientation"""
     sigma, thermostat, setup = get_sigmas_log(filename)
     sigma = np.rollaxis(sigma, 1).reshape(thermostat.size, -1)
-    for i in range(thermostat.size): hist(sigma[i], 64 , normed=True)
+    for i in range(thermostat.size):
+        hist(sigma[i], 64 , normed=True)
 
